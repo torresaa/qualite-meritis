f411f5759f6619fbb1a8f74e3cc4bf7d02547a4b
Matthieu Vergne <matthieu.vergne@meritis.fr>
2023-06-09 19:55:04 +0200

    Initialize first test based on description - Suggestion output
    As per the name shows, we expect to get some seats to be offered, so
    let's introduce the seat concept with its own class and make us capable
    of returning several of them.

--- src/fr/vergne/qualitemeritis/ProjectTest.java
+++ src/fr/vergne/qualitemeritis/ProjectTest.java
@@ -2,6 +2,8 @@ package fr.vergne.qualitemeritis;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Collection;
+
 import org.junit.jupiter.api.Test;
 
 class ProjectTest {
@@ -9,13 +11,17 @@ class ProjectTest {
 	@Test
 	void test() {
 		SuggestionSystem system = new SuggestionSystem();
-		system.offerBestSeats();
+		Collection<Seat> bestSeats = system.offerBestSeats();
+	}
+
+	static class Seat {
 	}
 
 	static class SuggestionSystem {
 
-		public void offerBestSeats() {
+		public Collection<Seat> offerBestSeats() {
 			// TODO
+			return null;
 		}
 	}
 }
