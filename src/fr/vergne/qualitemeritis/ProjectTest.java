1bbed3dd7bdc1045ec9e10f7a7fa1e2c9ecf04bf
Matthieu Vergne <matthieu.vergne@meritis.fr>
2023-06-09 19:40:39 +0200

    Initialize project
    We choose to go the test way: create iterative tests to validate the
    code, from simplest to most complex case.

---
+++ src/fr/vergne/qualitemeritis/ProjectTest.java
@@ -0,0 +1,13 @@
+package fr.vergne.qualitemeritis;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProjectTest {
+
+	@Test
+	void test() {
+	}
+
+}
